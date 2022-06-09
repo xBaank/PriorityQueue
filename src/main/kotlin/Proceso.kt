331cea1bfffcb6335d50cc72ec56a6ae3c0bc946
Bank <elroby444@gmail.com>
2022-06-09 11:14:36 +0200

    Hashset tests

--- src/main/kotlin/Proceso.kt
+++ src/main/kotlin/Proceso.kt
@@ -7,4 +7,22 @@ data class Proceso(val id: Int, val name: String, val prioridad: Int) : Comparab
         return -1
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Proceso
+
+        if (id != other.id) return false
+        if (name != other.name) return false
+        if (prioridad != other.prioridad) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return prioridad
+    }
+
+
 }
